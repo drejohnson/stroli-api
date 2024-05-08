50465dc4d8718c4c5826583e0a14f01d699997c3
Dre Johnson <dre@phreshr.com>
2024-05-07 20:03:03 -0400

    added globals

--- src/bin/httpd.rs
+++ src/bin/httpd.rs
@@ -1,14 +1,17 @@
 // dependencies
 use axum::{extract::Request, Router, ServiceExt};
-use edgedb_tokio::Builder;
-use edgedb_tokio::Client;
-use stroli_api_lib::domain::grab_secrets;
-use stroli_api_lib::domain::AppSecrets;
-use stroli_api_lib::domain::AppState;
+use edgedb_tokio::{Builder, Client, GlobalsDelta};
+use stroli_api_lib::domain::{grab_secrets, AppSecrets, AppState};
 use stroli_api_lib::errors::ApiError;
 use stroli_api_lib::startup::build_api_router;
 use tower_http::normalize_path::NormalizePathLayer;
 use tower_layer::Layer;
+
+#[derive(GlobalsDelta)]
+struct Globals<'a> {
+    #[edgedb(rename = "ext::auth::client_token")]
+    auth_token: &'a str,
+}
 
 // type to wrap a router and implement the Service trait for it
 struct StroliService {
@@ -54,6 +57,7 @@ async fn main(
 }
 
 async fn create_client(secrets: AppSecrets) -> Result<Client, ApiError> {
+    let token_from_auth_server: String = String::from("your_token_here");
     let cfg = Builder::new()
         .instance(&secrets.edgedb_instance)
         .unwrap()
@@ -63,6 +67,11 @@ async fn create_client(secrets: AppSecrets) -> Result<Client, ApiError> {
         .map_err(|e| ApiError::Database(e))?;
 
     let pool = Client::new(&cfg);
+
+    pool.with_globals(&Globals {
+        auth_token: token_from_auth_server.as_str(),
+    });
+
     pool.ensure_connected()
         .await
         .map_err(|e| ApiError::Database(e))?;
